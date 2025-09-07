bf5b1a9525293aa8411117526462fd243f7d7c46
GuBon <rnqhsrud01@gmail.com>
2025-09-08 07:46:44 +0900

    feat : 매장 공동구매 라우터 추가 #15

--- src/router/modules/business.js
+++ src/router/modules/business.js
@@ -1,4 +1,7 @@
 import BusinessPartnershipPage from '@/pages/BusinessPartnershipPage.vue'
+import BusinessHomePage from '@/pages/BusinessHomePage.vue'
+import BusinessGroupPurchasePage from '@/pages/BusinessGroupPurchasePage.vue'
+
 export default [
   {
     path: '/business',
@@ -6,8 +9,18 @@ export default [
     redirect: '/business/home'
   },
   {
+    path: '/business/home',
+    name: 'business-home',
+    component: BusinessHomePage,
+  },
+  {
     path: '/business/partnership',
     name: 'business-partnership',
     component: BusinessPartnershipPage,
+  },
+  {
+    path: '/business/group-purchase',
+    name: 'business-group-purchase',
+    component: BusinessGroupPurchasePage,
   },
 ]

36b8a829b2b9f02200bbd17a4d6d78b027ddca2f
GuBon <rnqhsrud01@gmail.com>
2025-09-07 22:54:59 +0900

    feat : 매장 페이지 라우터 수정 #8

--- src/router/modules/business.js
+++ src/router/modules/business.js
@@ -1,16 +1,9 @@
 import BusinessPartnershipPage from '@/pages/BusinessPartnershipPage.vue'
-import BusinessHomePage from '@/pages/BusinessHomePage.vue'
-
 export default [
   {
     path: '/business',
     name: 'business',
     redirect: '/business/home'
-  },
-  {
-    path: '/business/home',
-    name: 'business-home',
-    component: BusinessHomePage,
   },
   {
     path: '/business/partnership',

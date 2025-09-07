82001a2bca1287c0eeeecf13fb1d6bf77e0616f4
GuBon <rnqhsrud01@gmail.com>
2025-09-07 22:45:29 +0900

    feat : 매장 페이지 라우터 추가 #8

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import publicRoutes from './modules/public'
+import businessRoutes from './modules/business'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [...publicRoutes],
+  routes: [...publicRoutes, ...businessRoutes],
 })
 
 export default router

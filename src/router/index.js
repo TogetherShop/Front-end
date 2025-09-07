a6c7a9716eea84e21e7e8295235ff3e5390e31cd
GuBon <rnqhsrud01@gmail.com>
2025-09-08 08:02:06 +0900

    feat : index.js 수정 #15

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { jwtDecode } from 'jwt-decode'
 
 import businessRoutes from './modules/business'
-import routes from './modules/public'
+import publicRoutes from './modules/public'
 
-const routes: [...publicRoutes, ...businessRoutes]
-  
+const routes = [...publicRoutes, ...businessRoutes]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
